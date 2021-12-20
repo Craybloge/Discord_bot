22a849cc9b17c3166f4fc30c0e01d6f90dc32038
Mathieu Pithon <mpithon@predell.fr>
2021-12-20 11:45:45 +0100

    suppression .idea + update commande to global

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,7 +22,7 @@ const commands = [
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
 
