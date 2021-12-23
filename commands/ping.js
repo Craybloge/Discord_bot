e4267e16a9a0597dc55fe5d29615f44390804565
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2021-12-23 20:37:13 +0100

    fixing a crashing problem temporary

--- commands/ping.js
+++ commands/ping.js
@@ -14,10 +14,11 @@ module.exports = {
 				.setDescription('the user to ping')
 				.setRequired(true)),
 	async execute(interaction, client) {
-		client.users.get(interaction.options.getUser("user")["id"]).send({ files: ["..\\images\\pong.gif"] })
-		.catch(() => (client.channels.get(interaction.channel.id).send({ embeds: [erreurEnvoiMp] })));
-		return .author.send("message").catch(() => {
-			message.channel.send("User has DMs closed or has no mutual servers with the bot:(");
-		await interaction.reply(`<@${interaction.options.getUser("user")["id"]}> Pong!`);
+		// client.users.get(interaction.options.getUser("user")["id"]).send({ files: ["..\\images\\pong.gif"] })
+		// .catch(() => (client.channels.get(interaction.channel.id).send({ embeds: [erreurEnvoiMp] })));
+		// return .author.send("message").catch(() => {
+		// 	message.channel.send("User has DMs closed or has no mutual servers with the bot:(");
+		// await interaction.reply(`<@${interaction.options.getUser("user")["id"]}> Pong!`);
+		return interaction.reply("Rénovation en cours cette commande ne fonctionne pas")
 	},
 };
